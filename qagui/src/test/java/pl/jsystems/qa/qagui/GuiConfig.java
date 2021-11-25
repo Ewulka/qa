54b22ddbfc2d422970910e1cb5ef727e7a6c7924
ekuczera <EKuczera@i-s.com.pl>
2021-11-25 08:59:23 +0100

    nowy komentarz

---
+++ qagui/src/test/java/pl/jsystems/qa/qagui/GuiConfig.java
@@ -0,0 +1,54 @@
+package pl.jsystems.qa.qagui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class GuiConfig {
+
+    protected WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+//        WebDriverManager.chromedriver().setup();
+     WebDriverManager.edgedriver().setup();
+       // WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+//        try {
+//            System.setProperty("webdriver.gecko.driver", Paths.get(getClass().getClassLoader()
+//                    .getResource("drivers/geckodriver.exe").toURI()).toFile().getAbsolutePath());
+//
+//            System.setProperty("webdriver.chrome.driver", Paths.get(getClass().getClassLoader()
+//                    .getResource("drivers/chromedriver.exe").toURI()).toFile().getAbsolutePath());
+//            System.setProperty("webdriver.edge.driver", Paths.get(getClass().getClassLoader()
+//                    .getResource("driver/msedgedriver.exe").toURI()).toFile().getAbsolutePath());
+//
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+
+
+       // driver = new FirefoxDriver();
+      driver = new EdgeDriver();
+//        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
