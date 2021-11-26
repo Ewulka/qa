0cf2527aad34dda2b48a4a28f1a8b242cdc8d4c9
ekuczera <EKuczera@i-s.com.pl>
2021-11-26 09:19:25 +0100

    nowy komentarz

--- qagui/src/test/java/pl/jsystems/qa/qagui/bdd/RunTest.java
+++ qagui/src/test/java/pl/jsystems/qa/qagui/bdd/RunTest.java
@@ -1,33 +1,56 @@
 package pl.jsystems.qa.qagui.bdd;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.*;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources",
-        glue = "classpath:pl.jsystems.qa.qagui.bdd",
-        plugin = {"pretty", "summary", "html:target/cucumber/report.html", "json:target/cucumber.json",
-                "junit:target/cucumber.xml", "rerun:target/rerun.txt"},
-        tags =
-//                "@BDD "
-//                        +
-//                        "and "
-//                        +
-//                        "@Login "
-//                        +
-//                        "and "
-//                        +
-//                        "@wordpress "
-//                        +
-//                        "and "
-//                        +
-//                        "@userpanel"
-//        "@website"
-//        "@website2"
-                "@website3"
-)
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "src/test/resources",
+//        glue = "classpath:pl.jsystems.qa.qagui.bdd",
+//        plugin = {"pretty", "summary", "html:target/cucumber/report.html", "json:target/cucumber.json",
+//        "junit:target/cucumber.xml", "rerun:target/rerun.txt"},
+//        tags =
+////                "@BDD "
+////                        +
+////                        "and "
+////                        +
+////                        "@Login "
+////                        +
+////                        "and "
+////                        +
+////                        "@wordpress "
+////                        +
+////                        "and "
+////                        +
+////                        "@userpanel"
+////        "@website"
+////        "@website2"
+//        "@website3"
+//)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "pl.jsystems.qa.qagui.bdd")
+@IncludeTags({
+//        "BDD"
+//        ,
+//        "BDD-NO-PARAM"
+//        ,
+//        "BDD-PARAM"
+//        ,
+        "Login"
+//        ,
+//        "wordpress"
+//        ,
+//        "userpanel"
+//        ,
+//        "website"
+//        ,
+//        "website2"
+//        ,
+//        "website3"
+})
 public class RunTest {
 
 
